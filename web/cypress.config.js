573b38effe4756e813214db0ee41db5446d2851c
Marcelo <marcelo@alluresecurity.com>
2025-12-22 20:39:23 -0300

    Configured cucumber

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/tests/features/**/*.feature",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
