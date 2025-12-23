8b8eb1168e621ce57d450572f2a7e3d234a5008b
sibarra <ibarra.sebastian.et12@gmail.com>
2025-12-23 00:09:24 -0300

    updated locator

--- web/cypress/src/locators/CartPage.locators.js
+++ web/cypress/src/locators/CartPage.locators.js
@@ -2,6 +2,6 @@ export const CartPageLocators = {
   PRODUCTS_HEADING: '#page-wrapper h2:contains("Products")',
   PRODUCT_IN_CART: '#tbodyid tr td',
   CART_ROWS: '#tbodyid tr',
-  PLACE_ORDER_BUTTON: '[data-target="#orderModal"]'
+  PLACE_ORDER_BUTTON: '[data-target="#orderModal"]:contains("Place Order")'
 };
 
