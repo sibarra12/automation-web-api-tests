825cc92918c8fd83c383ad9d430e33f57dcfda17
Marcelo <marcelo@alluresecurity.com>
2025-12-22 21:18:31 -0300

    Added When step

--- web/cypress/tests/steps/buy-products.js
+++ web/cypress/tests/steps/buy-products.js
@@ -1,6 +1,19 @@
-const { Given } = require("@badeball/cypress-cucumber-preprocessor");
+const { Given, When } = require("@badeball/cypress-cucumber-preprocessor");
+const productsPage = require("../../src/pages/ProductsPage");
+const productDetailPage = require("../../src/pages/ProductDetailPage");
 
 Given("the user enters the Demoblaze platform", () => {
   cy.visit("https://demoblaze.com");
 });
 
+When("the user adds products to the cart", (dataTable) => {
+  const products = dataTable.hashes();
+  
+  products.forEach((row) => {
+    productsPage.selectProductByName(row.productName);
+    cy.setupAlertHandler();
+    productDetailPage.addToCart();
+    cy.verifyAlertMessage('Product added');
+    cy.visit("https://demoblaze.com");
+  });
+});
