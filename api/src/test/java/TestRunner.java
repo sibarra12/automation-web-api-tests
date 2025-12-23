d506978ff1b3616ddf89761b7715a3aa8d68f0bc
sibarra <ibarra.sebastian.et12@gmail.com>
2025-12-23 02:44:59 -0300

    added tags implementation

--- api/src/test/java/TestRunner.java
+++ api/src/test/java/TestRunner.java
@@ -4,6 +4,10 @@ public class TestRunner {
     
     @Karate.Test
     Karate testAll() {
+        String tags = System.getProperty("tags");
+        if (tags != null && !tags.isEmpty()) {
+            return Karate.run("classpath:features").tags(tags);
+        }
         return Karate.run("classpath:features");
     }
 }
