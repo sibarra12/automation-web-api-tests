59aba36985feec5ff7f758e9082641c01b27b571
sibarra <ibarra.sebastian.et12@gmail.com>
2025-12-22 23:18:29 -0300

    created new page function to click place order button

--- web/cypress/src/pages/CartPage.js
+++ web/cypress/src/pages/CartPage.js
@@ -12,6 +12,10 @@ class CartPage {
     cy.get('#tbodyid tr').should('have.length', count);
   }
   
+  clickPlaceOrder() {
+    cy.get('[data-target="#orderModal"]').contains('Place Order').click();
+  }
+  
 }
 
 module.exports = new CartPage();
