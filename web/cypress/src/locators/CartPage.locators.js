9c619e7a83c51d75eabfe1fcb24b0cefa24b1869
sibarra <ibarra.sebastian.et12@gmail.com>
2025-12-22 23:55:03 -0300

    created locator file by cart page

---
+++ web/cypress/src/locators/CartPage.locators.js
@@ -0,0 +1,7 @@
+export const CartPageLocators = {
+  PRODUCTS_HEADING: '#page-wrapper h2:contains("Products")',
+  PRODUCT_IN_CART: '#tbodyid tr td',
+  CART_ROWS: '#tbodyid tr',
+  PLACE_ORDER_BUTTON: '[data-target="#orderModal"]'
+};
+
