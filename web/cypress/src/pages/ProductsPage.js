3e12b5f1cc6a5020d2e27124b13bf1ef450140ce
Marcelo <marcelo@alluresecurity.com>
2025-12-22 23:00:13 -0300

    added page function by navigation

--- web/cypress/src/pages/ProductsPage.js
+++ web/cypress/src/pages/ProductsPage.js
@@ -4,6 +4,10 @@ class ProductsPage {
     cy.get('#tbodyid .card-block .card-title').contains(productName).click();
   }
   
+  navigateToCart() {
+    cy.get('#navbarExample ul.navbar-nav li.nav-item :contains("Cart")').click();
+  }
+  
 }
 
 module.exports = new ProductsPage();
