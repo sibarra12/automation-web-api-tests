d6e378e6a0be33d470ffe29464df7fd28fb1fed0
sibarra <ibarra.sebastian.et12@gmail.com>
2025-12-23 02:19:56 -0300

    created test runner

---
+++ api/src/test/java/demoblaze/api/TestRunner.java
@@ -0,0 +1,12 @@
+package demoblaze.api;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+    
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:features/*.feature");
+    }
+}
+
